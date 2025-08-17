0646fab09f0400bdfc4be9b17f4500af24cf7d34
Gabriel <gabfinsoa@gmail.com>
2025-08-17 22:04:20 +0100

    Scanner_In_and_Constant_Variable

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+
+    private final static String WELCOME_MSG = "Welcome to Scanner App";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(WELCOME_MSG);
+        System.out.print("Name: ");
+        String name = scanner.next();
+        System.out.print("Age:  ");
+        int age = scanner.nextInt();
+        System.out.println("Custumer details:  " + name + ", " + age);
+        System.out.printf("Custumer details:  %s, %s", name, age);
     }
 }
