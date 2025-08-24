92acb67aaa40a2d933cb18821102f54f5d316078
Gabriel <gabfinsoa@gmail.com>
2025-08-24 21:52:45 +0100

    Clean_main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,20 +6,6 @@ public class Main {
 
     private final static String WELCOME_MSG = "Welcome to Scanner App";
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {}
 
-        System.out.print("Add the first number:  ");
-        var number1 = scanner.nextFloat();
-        System.out.print("Add the second number: ");
-        var number2 = scanner.nextFloat();
-        System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
-        System.out.printf("%s + %s = %s\n", number1, number2, number1 + number2);
-        System.out.printf("%s - %s = %s\n", number1, number2, number1 - number2);
-        System.out.printf("%s / %s = %s\n", number1, number2, number1 / number2);
-        System.out.printf("%s * %s = %s\n", number1, number2, number1 * number2);
-        System.out.printf("%s %% %s = %s\n", number1, number2, number1 % number2);
-        System.out.printf("Raiz quadrada de %s eh %s\n", number1, Math.sqrt(number1));
-        System.out.printf("A potencia de %s ao %s eh %s\n", number1, number2, Math.pow(number1, number2));
-    }
 }
