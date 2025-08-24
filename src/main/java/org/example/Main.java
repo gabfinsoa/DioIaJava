b8e8e92ec6a8b5493e6f2567c78effed30f9f84f
Gabriel <gabfinsoa@gmail.com>
2025-08-24 19:47:36 +0100

    operadores_matematicos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,12 +8,18 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(WELCOME_MSG);
-        System.out.print("Name: ");
-        String name = scanner.next();
-        System.out.print("Age:  ");
-        int age = scanner.nextInt();
-        System.out.println("Custumer details:  " + name + ", " + age);
-        System.out.printf("Custumer details:  %s, %s", name, age);
+
+        System.out.print("Add the first number:  ");
+        var number1 = scanner.nextFloat();
+        System.out.print("Add the second number: ");
+        var number2 = scanner.nextFloat();
+        System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+        System.out.printf("%s + %s = %s\n", number1, number2, number1 + number2);
+        System.out.printf("%s - %s = %s\n", number1, number2, number1 - number2);
+        System.out.printf("%s / %s = %s\n", number1, number2, number1 / number2);
+        System.out.printf("%s * %s = %s\n", number1, number2, number1 * number2);
+        System.out.printf("%s %% %s = %s\n", number1, number2, number1 % number2);
+        System.out.printf("Raiz quadrada de %s eh %s\n", number1, Math.sqrt(number1));
+        System.out.printf("A potencia de %s ao %s eh %s\n", number1, number2, Math.pow(number1, number2));
     }
 }
