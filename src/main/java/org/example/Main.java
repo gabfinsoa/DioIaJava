f47fe02aceb747f2bae821ae332b68269c15d775
Gabriel <gabfinsoa@gmail.com>
2025-08-24 21:49:29 +0100

    Receive_year_return_user_age

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,22 +4,18 @@ import java.util.Scanner;
 
 public class Main {
 
-    private final static String WELCOME_MSG = "Welcome to Scanner App";
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Add the first number:  ");
-        var number1 = scanner.nextFloat();
-        System.out.print("Add the second number: ");
-        var number2 = scanner.nextFloat();
-        System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
-        System.out.printf("%s + %s = %s\n", number1, number2, number1 + number2);
-        System.out.printf("%s - %s = %s\n", number1, number2, number1 - number2);
-        System.out.printf("%s / %s = %s\n", number1, number2, number1 / number2);
-        System.out.printf("%s * %s = %s\n", number1, number2, number1 * number2);
-        System.out.printf("%s %% %s = %s\n", number1, number2, number1 % number2);
-        System.out.printf("Raiz quadrada de %s eh %s\n", number1, Math.sqrt(number1));
-        System.out.printf("A potencia de %s ao %s eh %s\n", number1, number2, Math.pow(number1, number2));
+        Scanner scanner = new Scanner(System.in);
+        var currentYear = 2025;
+
+        System.out.printf("What's your name? ");
+        var name = scanner.next();
+        System.out.printf("When did you born (YYYY)? ");
+        var year = scanner.nextInt();
+
+        var ageResult = currentYear - year;
+
+        System.out.printf("Hi %s, you are %s years old.", name, ageResult);
     }
 }
