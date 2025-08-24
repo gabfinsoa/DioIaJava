6f6c83ceeb1e8cafac3b3f109aa8989550bc3fe3
Gabriel <gabfinsoa@gmail.com>
2025-08-24 22:03:16 +0100

    calculate_square_area

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,14 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("Add the Square side: ");
+        var squareSide = scanner.nextInt();
+        squareSide *= squareSide;
+        System.out.println("Square area = " + squareSide);
+
+    }
 
 }
