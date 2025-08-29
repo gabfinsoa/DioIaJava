4ae581731210e58f9873eea6b9036a109310ea2b
Gabriel <gabfinsoa@gmail.com>
2025-08-29 09:56:31 +0100

    calculate_age_difference

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,28 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        var ageDifference = 0;
+
+        System.out.printf("Person 1 - Name: ");
+        var name1 = scanner.next();
+        System.out.printf("Person 1 -  Age: ");
+        var age1 = scanner.nextInt();
+        System.out.printf("Person 2 - Name: ");
+        var name2 = scanner.next();
+        System.out.printf("Person 2 -  Age: ");
+        var age2 = scanner.nextInt();
+
+        if (age1 > age2) {
+            ageDifference = age1 - age2;
+        } else {
+            ageDifference = age2 - age1;
+        }
+
+        System.out.printf("Age difference = " + ageDifference);
+
+
+    }
 
 }
