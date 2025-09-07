a021447485bc6bd011e2db2ea639a29c06d74e31
Gabriel <gabfinsoa@gmail.com>
2025-09-07 20:45:09 +0100

    if_else_elseif_class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,28 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+
+        var isEmancipated = false;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Insert your name: ");
+        var name = scanner.next();
+        System.out.printf("Insert your age:  ");
+        var age = scanner.nextInt();
+        if (age < 18) {
+            System.out.printf("Are you emancipated? (s/n): ");
+            isEmancipated = scanner.next().equalsIgnoreCase("s");
+        }
+
+        if (age >= 18) {
+            System.out.printf("%s, you are %s years old so you can drive!\n", name, age);
+        } else if (age >= 16 && isEmancipated) {
+            System.out.printf("%s, you are %s years old and Emancipated, so you can drive!\n", name, age);
+        } else {
+            System.out.printf("Sorry %s, you can't drive yet.\n", name);
+        }
+        System.out.println("-* Program finished *-");
+    }
 
 }
