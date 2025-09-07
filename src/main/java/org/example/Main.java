a7db61e59f03ee7598a4ac23f1487fd8d2425afa
Gabriel <gabfinsoa@gmail.com>
2025-09-07 21:26:24 +0100

    switch_case_normal

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,33 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Select a number between 1 and 6: ");
+        var option = scanner.nextInt();
+        switch (option) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Weekend!!!");
+                break;
+            default:
+                System.out.println("Invalid option");
+        }
+    }
 
 }
