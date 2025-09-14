7ecccb2b306a151e1c77bd59feae03646071714f
Gabriel <gabfinsoa@gmail.com>
2025-09-14 21:07:58 +0100

    for_printPares

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        // Imprimir pares   ou    i+=2
+        for (var i = 1; i <= 100; i++) {
+            if (i % 2 == 1) continue;
+            System.out.println("Number: " + i);
+        }
+
+        // Print name till the name be equal to "Exit"
         Scanner scanner = new Scanner(System.in);
         for (;;) {
             System.out.printf("Your name: ");
