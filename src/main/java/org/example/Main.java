907b9173b9e7c8c5516e2694a79cee930bcd8605
Gabriel <gabfinsoa@gmail.com>
2025-09-14 20:16:38 +0100

    for_nameConditionToExit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,15 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for (;;) {
+            System.out.printf("Your name: ");
+            var name = scanner.next();
+
+            if (name.equalsIgnoreCase("exit")) break;
+            System.out.println("Name: " + name);
+        }
+    }
 
 }
