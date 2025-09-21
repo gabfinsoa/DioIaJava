d0293ffe14163cf6309797423198c794822df9a7
Gabriel <gabfinsoa@gmail.com>
2025-09-21 19:22:01 +0100

    whileDo_DoWhile_Class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,34 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = "";
+
+        do {
+            System.out.printf("Add your name: ");
+            name = scanner.next();
+            System.out.println("Wrote name  ->  " + name);
+
+            if (name.equalsIgnoreCase("exit")) break;
+        } while (name.equalsIgnoreCase("exit"));
+
+        /*
+         while (!name.equalsIgnoreCase("exit")){
+            System.out.printf("Add your name: ");
+            name = scanner.next();
+            System.out.println("Wrote name  ->  " + name);
+        }
+
+        while (true){
+            System.out.printf("Add your name: ");
+            name = scanner.next();
+            System.out.println("Wrote name  ->  " + name);
+
+            if (name.equalsIgnoreCase("exit")) break;
+        }
+
+        */
+    }
 
 }
